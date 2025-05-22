be11e982004110222a3b5168f8600766ab976acd
flora <mclaradev@gmail.com>
2025-05-22 17:55:21 -0300

    tipos primitivos e saída de dados

---
+++ ex002.py
@@ -0,0 +1,14 @@
+# tipos primitivos e saídas de dadoos
+
+# n1 = input("Digite um valor: ")
+# n1 = int(input("Digite um valor: "))
+# n1 = float(input("Digite um valor: "))
+# n1 = bool(input("Digite um número: "))
+# print(type(n1))
+
+# somando os valores usando python moderno
+
+n1 = int(input("Digite um valor: "))
+n2 = int(input("Digite outro valor:"))
+s = n1 + n2
+print(f"A soma entre {n1} e {n2} é igual a: {s}")
