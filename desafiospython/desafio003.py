ea345e6e7336a3456eee5079d5feb8439382dc1e
flora <mclaradev@gmail.com>
2025-05-22 18:07:34 -0300

    desafio python: crie um programa que leia dois números e mostre a soma entre eles.

---
+++ desafiospython/desafio003.py
@@ -0,0 +1,6 @@
+print("Seja bem-vind@ ao python!")
+
+n1 = int(input("digite qualquer valor:"))
+n2 = int(input("digite outro valor:"))
+s = n1 + n2
+print(f'A soma entre {n1} e {n2} é igual a {s}')
